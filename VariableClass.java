88589933d81c9ec9669a615b6fcff45b8d344bb0
abishekn1 <abishekraj0405@gmail.com>
2023-06-19 13:21:37 +0530

    second-push

---
+++ VariableClass.java
@@ -0,0 +1,28 @@
+public class VariableClass{
+
+	public static void main(String[] args){
+
+		int totalNumberOfStudents = 20;
+
+		totalNumberOfStudents = 30;
+
+	System.out.println(totalNumberOfStudents);
+
+
+
+
+	}
+
+
+	
+
+
+
+
+	
+
+
+
+
+	
+}
