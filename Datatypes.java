a83aac9bf4ccf92db189c2f2fd59d3beaa162063
abishekn1 <abishekraj0405@gmail.com>
2023-06-19 13:07:22 +0530

    test-push

---
+++ Datatypes.java
@@ -0,0 +1,79 @@
+public class Datatypes{
+	
+	public static void main(String[] args){
+
+
+		byte totalNumberOfStates = 32;
+		byte numberOfVowels = 5;
+		byte numberOfAlphabets = 26;
+		byte numberOfHoursInADay = 24;
+		byte numberOfDaysInAWeek = 7;
+		byte sidesOfSqaure = 4;
+		byte coloursInRainbow = 7;
+		byte roomTemperature = 100;
+		byte numberDirection;
+
+
+		short pGRent = 10000;
+		short costOfGoldInGrams = 5000;
+		short TotalMarks = 625;
+		short costOfFan = 4000;
+		short distanceFromBangaloreToManali = 2000;
+		short numberSixesInIpl2023 = 1020;
+
+
+		int distanceSunAndEarth = 150000000;
+		int speedOfLight = 300000;
+		int populationOfIndia = 14200000;
+		int salaryOfSundarPichai = 1600000;
+		int costOfApartment = 1000000;
+
+		long worldPopulation = 20000000000L;
+		long phoneNumber = 9786553586L;
+		long lanLineNumber = 036261616166L;
+		long aadhaarNumber = 654645665656L;
+		long accNumber = 8778757547678L;
+
+
+		float gravityOfEarth = 9.8F;
+		float valueOfPie = 3.142F;
+		float cgpa = 7.9F;
+		float kholiAverage = 50.7F;
+		float versionOfJava = 1.8F;
+
+		double speedOfLight = 300.767573573D;
+
+		char grade = 'A';
+		char divisionOfClass = 'C';
+		char gender = 'M';
+		char neutralSign = 'N';
+		char size = 'S';
+
+
+		boolean todayIsFriday = true;
+		boolean fiveIsEven = false;
+		boolean isItRaining = false;
+		boolean isItSummer = true;
+		boolean isSundayHoliday = false;
+		boolean isJavaUnderStanding = false;
+		boolean isClassOver = false;
+		
+
+
+
+
+		System.out.println(gravityOfEarth);
+
+
+
+
+
+
+	
+
+
+
+	}
+
+
+}
